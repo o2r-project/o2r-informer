05b1b64a5bd7cae51a538025deb0908cd3129d4d
Jan Koppe <post@jankoppe.de>
2016-08-29 12:46:07 +0200

    updated example html file

--- index.js
+++ index.js
@@ -71,25 +71,6 @@ merge = (obj1, obj2) => {
 // emit changes through socket.io
 watcher.watch(config.mongo.database + '.jobs', event => {
   if (event.operation === 'update') {
-
-    // This code needs a lot more logic! Two types of events can happen:
-    // * partial (field) update
-    // * complete (whole document) update
-    //
-    // partial update:
-    // In a partial update, the event.data will only hold the $set object with a field
-    // that is named after the key, and its new value. You can't access it like event.data.$set.a.b.c,
-    // because the field-name is a string, so you need to do event.data.$set["a.b.c"].
-    // TODO: This should probably be rebuilt to a real object for consistency.
-    //
-    // also, it will not contain the Job ID, which is saved in the 'id' key. It will contain the targetId though,
-    // which references the MongoDB document by its internal primary key '_id'. Through this, we can findById our
-    // Document again and from there extract the necessary Job ID and other things that might be relevant.
-    //
-    // complete update:
-    // In a complete update, the document has been saved as a whole. The event.data will then hold the new, complete
-    // document, so things like event.data.id are already defined.
-    //
     if (event.data.$set) {
       // only partial update, retrieve the job id from the complete document
       Job.findById(event.targetId, (err, job) => {
